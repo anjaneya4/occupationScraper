847fa2e0e3898e718bd3854d76662f87fb962fbf
anjaneya4 <anjaneya4@gmail.com>
2014-09-07 13:31:42 +0530

    added support for init kids, maxcount to control iterations, prints node info, corrected parentis and isleafnode logic. no support for seed script,

--- scrape.py
+++ scrape.py
@@ -1,12 +1,21 @@
 import urllib2
 import string
+import StringIO
 RESOURCE_BASE='http://www.occupationalinfo.org/'
+IGNORE_BASIS=' cat_div_0.htmlOccupational Categories, Divisions, and Groups'
 from HTMLParser import HTMLParser
 from htmlentitydefs import name2codepoint
 parentid=None
-level=0
+count=0
+countmax=10
 class MyHTMLParser(HTMLParser):
     def __init__(self,basis):
+        global count
+        print 'count: ',count
+        if count==countmax:
+            quit()
+        if count<=countmax:
+            count+=1
         HTMLParser.__init__(self)
         self.decodebasis(basis)
         responsecache=urllib2.urlopen(self.url)
@@ -14,23 +23,39 @@ class MyHTMLParser(HTMLParser):
         responsecache.close()
         self.rawscrp=''
         self.feed(self.httpresponse)
-        self.kids=None
-        self.level=level
-        self.isleaf=True
-#        self.initnode(self)
+        if count==1:
+            self.rawscrp=string.strip(self.rawscrp,IGNORE_BASIS)
+        self.rawscrp=string.strip(self.rawscrp)
+        self.kids=[]
+        self.isleaf=False
+        self.initnode()
     def handle_starttag(self, tag, attrs):
         for name, value in attrs:
-            if value is not None and value.startswith("cat") or value is not None and value.startswith('defset'):
+            #print value
+            if value is not None and (value.startswith('./') or value.startswith("cat") or value.startswith('defset')):
                 self.rawscrp+=value.strip()
     def handle_data(self, data):
-        self.rawscrp+=data
+        if self.rawscrp[-5:] == '.html':
+            clipindex=string.find(data,'(')
+            data=data[:clipindex]
+            data=string.strip(data)
+            data+='\n'
+            self.rawscrp+=data
+            
     def initnode(self):
-        self.filtercontents(self)
-        
+        if not self.rawscrp:
+            self.isleaf=True
+        print 'isleaf: ',self.isleaf
+        s = StringIO.StringIO(self.rawscrp) 
+        for line in s:
+            if not self.isleaf:
+                kid= MyHTMLParser(line)
+                kid.parentid=self.id
+                self.kids.append(kid)
     
         
     def decodebasis(self,basis):
-        print 'decpoding basis: ',basis
+        print '>>>InitNode: decoding basis: ',basis
         basis=string.strip(basis)
         #set id
         datastart=string.find(basis,'.html')+5
@@ -56,9 +81,6 @@ class MyHTMLParser(HTMLParser):
         self.parentid=parentid
         print 'parentid: ',self.parentid
         parentid=self.id
-#    def handle_kids
 parser = MyHTMLParser('contents.html0 - Occupational Categories, Divisions, and Groups')
 print parser.rawscrp
-print parser.id
-print parser.value
 parser.close()
