9f3613f96d3722f9c6d4196c1019dcb0b63180e5
anjaneya4 <anjaneya4@gmail.com>
2014-09-08 11:56:01 +0530

    pickling/dumping scraped values to local file to avoid multiple fetch

--- scrape.py
+++ scrape.py
@@ -1,4 +1,4 @@
--# Author:Anjaneya Jha <anjaneya4@gmail.com>
+import pickle
 import urllib2
 import string
 import StringIO
@@ -8,28 +8,31 @@ from HTMLParser import HTMLParser
 from htmlentitydefs import name2codepoint
 parentid=None
 count=0
-countmax=10
+countmax=100
 class MyHTMLParser(HTMLParser):
     def __init__(self,basis):
         global count
         print 'count: ',count
-        if count==countmax:
-            quit()
-        if count<=countmax:
-            count+=1
+        # if count==countmax:
+        #     quit()
+        count+=1
         HTMLParser.__init__(self)
         self.decodebasis(basis)
-        responsecache=urllib2.urlopen(self.url)
-        self.httpresponse=responsecache.read()
-        responsecache.close()
-        self.rawscrp=''
-        self.feed(self.httpresponse)
-        if count==1:
-            self.rawscrp=string.strip(self.rawscrp,IGNORE_BASIS)
-        self.rawscrp=string.strip(self.rawscrp)
-        self.kids=[]
-        self.isleaf=False
-        self.initnode()
+        # print self.id,'||',self.parentid, '||',self.value
+        if not self.url==None:
+            global parentid
+            parentid=self.parentid
+            responsecache=urllib2.urlopen(self.url)
+            self.httpresponse=responsecache.read()
+            responsecache.close()
+            self.rawscrp=''
+            self.feed(self.httpresponse)
+            if count==1:
+                self.rawscrp=string.strip(self.rawscrp,IGNORE_BASIS)
+            self.rawscrp=string.strip(self.rawscrp)
+            self.kids=[]
+            self.isleaf=False
+            self.initnode()
     def handle_starttag(self, tag, attrs):
         for name, value in attrs:
             #print value
@@ -46,7 +49,7 @@ class MyHTMLParser(HTMLParser):
     def initnode(self):
         if not self.rawscrp:
             self.isleaf=True
-        print 'isleaf: ',self.isleaf
+        # print 'isleaf: ',self.isleaf
         s = StringIO.StringIO(self.rawscrp) 
         for line in s:
             if not self.isleaf:
@@ -56,7 +59,7 @@ class MyHTMLParser(HTMLParser):
     
         
     def decodebasis(self,basis):
-        print '>>>InitNode: decoding basis: ',basis
+        #print '>>>InitNode: decoding basis: ',basis
         basis=string.strip(basis)
         #set id
         datastart=string.find(basis,'.html')+5
@@ -64,7 +67,9 @@ class MyHTMLParser(HTMLParser):
         path=basis[:datastart]
         #print 'path: ',path
         self.url=RESOURCE_BASE+path
-        print 'url: ',self.url
+        if '/./' in self.url:
+            self.url=None
+        #print 'url: ',self.url
         data=basis[datastart:]
         data=string.strip(data)
         #print 'data: ',data
@@ -72,16 +77,31 @@ class MyHTMLParser(HTMLParser):
         #print 'delimpos: ',delimpos
         self.id=data[:delimpos]
         self.id=string.strip(self.id)
-        print 'id: ',self.id
+        #print 'id: ',self.id
         rawvalue=data[delimpos:]
         rawvalue=string.strip(rawvalue)
         rawvalue=string.strip(rawvalue,'-')
         self.value=string.strip(rawvalue)
-        print 'value: ',self.value
+        #print 'value: ',self.value
         global parentid
         self.parentid=parentid
-        print 'parentid: ',self.parentid
+        #print 'parentid: ',self.parentid
         parentid=self.id
+    def traverse(self):
+        print self.id,'||', self.parentid,'||',self.value
+        if not self.url==None:
+            for kid in self.kids:
+                kid.traverse()
+        # clearing dump overload since not required
+        self.httpresponse=None
+        self.rawscrp=None
 parser = MyHTMLParser('contents.html0 - Occupational Categories, Divisions, and Groups')
-print parser.rawscrp
+parser.traverse()
+filename='/home/anjaneya/software/utils/occupationScraper/scrapepickledump.pk1'
+with open(filename, 'wb') as output:
+        pickle.dump(parser, output, pickle.HIGHEST_PROTOCOL)
 parser.close()
+# filename='/home/anjaneya/software/utils/occupationScraper/scrapepickledump.pk1'
+# with open(filename, 'rb') as input:
+#     parser=pickle.load(input)
+#     parser.traverse()
